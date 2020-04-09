d1c42ea562991b73f652b3e384de0cd225e90d0b
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 04:24:01 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,17 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  #Initialize with a string URL
+  def initialize
+    
+  end
+
+  #get_response_body method that sends GET request to the URL
+  # -> Return the body of the response
+
+  #parse_json method that uses get_response_body method to send a request
+  # Return Ruby Hash of data converted
+end
