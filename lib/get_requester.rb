3b3762573ae7dac8197f8e65a3db5e0b4fff2ebd
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 04:24:13 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -11,7 +11,11 @@ class GetRequester
 
   #get_response_body method that sends GET request to the URL
   # -> Return the body of the response
+  def get_response_body
+  end
 
   #parse_json method that uses get_response_body method to send a request
   # Return Ruby Hash of data converted
+  def parse_json
+  end
 end
