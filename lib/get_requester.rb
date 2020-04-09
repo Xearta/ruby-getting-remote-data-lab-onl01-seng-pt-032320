c38a7c057c3f37248bdeb3e18f72ea8b10f58182
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 04:27:49 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  #Initialize with a string URL
+  def initialize(url)
+    #@URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    @url = url
+  end
+
+  #get_response_body method that sends GET request to the URL
+  # -> Return the body of the response
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  #parse_json method that uses get_response_body method to send a request
+  # Return Ruby Hash of data converted
+  def parse_json
+    people = JSON.parse(self.get_response_body)
+  end
+end
